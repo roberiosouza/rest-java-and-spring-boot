5666966c47ace3a7600515c3630aa8d7e76af63a
roberio.dev <rroberiosouza@gmail.com>
2022-12-19 22:00:52 -0300

    Classe UnsupportedMathOperationException
    Implementado classe que retorna a exceção gerada.

---
+++ first-steps-java-spring-boot/rest-java-and-spring-boot/src/main/java/br/com/rbsystem/exceptions/UnsupportedMathOperationException.java
@@ -0,0 +1,15 @@
+package br.com.rbsystem.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException {
+	
+	public UnsupportedMathOperationException(String ex) {
+		super(ex);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
